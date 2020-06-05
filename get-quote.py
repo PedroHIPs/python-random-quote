26232dbe9135d126d876df8e91dc7cd70f9e3867
PedroHIPs <pedroevs.0405@gmail.com>
2020-06-05 11:23:30 -0300

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -3,6 +3,11 @@ import random;
 def initial():
   #print("Keep it logically awesome.")
 
+  i = int(1)
+  f = open("quotes.txt", "a")
+  f.write("Now the file has more quotes!")
+  f.close()
+
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
@@ -10,8 +15,10 @@ def initial():
   last = len(quotes) - 1
   
   rnd = random.randint(0, last);
+  Newrnd = random.randint(0, last);
 
-  print(quotes[rnd])
+  print(quotes[rnd],quotes[Newrnd])
+
 
 if __name__== "__main__":
   initial()
